a7e311ff8c1b726df272b4d5670fa1727fe19d52
Sebastian Gracia <sebasgraciavalderrama@users.noreply.github.com>
2021-12-16 14:34:11 -0500

    Question about custom filters code snippet

--- filter examples.py
+++ filter examples.py
@@ -1,4 +1,6 @@
 def filter(value):
+    # I believe this function gets called for each record of the dataset, based on the input and the output.
+    # Q: Do we always have to use dictonaries for this type of functions? Can we use, case statements, if conditionals, etc...
     schoolDistrict = { 'WCES':'100', 'WCMS':'200', 'WCHS':'300'}
 
     if value in schoolDistrict:
