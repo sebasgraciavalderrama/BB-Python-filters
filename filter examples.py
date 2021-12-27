7c644b00f0964515bacd59358c0d09205be9db05
Sebastian Gracia <sebastiangracia123@gmail.com>
2021-12-27 16:41:42 -0500

    Typos and testing new IDE

--- filter examples.py
+++ filter examples.py
@@ -1,6 +1,6 @@
 def filter(value):
     # I believe this function gets called for each record of the dataset, based on the input and the output.
-    # Q: Do we always have to use dictonaries for this type of functions? Can we use, case statements, if conditionals, etc...
+    # Q: Do we always have to use dictionaries for this type of functions? Can we use, case statements, if conditionals, etc...
     schoolDistrict = { 'WCES':'100', 'WCMS':'200', 'WCHS':'300'}
 
     if value in schoolDistrict:
@@ -13,10 +13,9 @@ def filter(value):
 #Role IDs
 
 def filter(value):
-    roleDicstrict = { 'TEA':'8', 'TEACH':'8', 'PRIN':'6'}
-
-    if value in roleDicstrict:
-        value = roleDicstrict.get(value)
+    roleDistrict = { 'TEA':'8', 'TEACH':'8', 'PRIN':'6'}
+    if value in roleDistrict:
+        value = roleDistrict.get(value)
     else:
         value = '10'
     result = value
